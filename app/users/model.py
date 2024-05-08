d7098a9415d6e9e75d0e75def7eaa9c4f775427f
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-05-08 11:28:22 +0300

    add models

--- app/users/model.py
+++ app/users/model.py
@@ -5,9 +5,9 @@ from sqlalchemy import Column, String, Integer
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String)
     last_name = Column(String)
-    passport_id = Column(String, nullable=False)
-    phone_number = Column(String, nullable=False)
+    passport_id = Column(String)
+    phone_number = Column(String)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
