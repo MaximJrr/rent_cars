e4e0f12b2a461c4849d36225b3cb8558313a93a1
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-05-08 11:31:32 +0300

    add routers

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from app.rents.router import router as router_rents
+from app.cars.router import router as router_cars
+from app.users.router import router as router_users
 
 app = FastAPI()
 
-
-@app.get('/cars')
-def get_cars():
-    pass
+app.include_router(router_rents)
+app.include_router(router_cars)
+app.include_router(router_users)
