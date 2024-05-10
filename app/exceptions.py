38c43bf0d6145e6f41711c83af410a095d933e85
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-05-10 13:39:04 +0300

    add custom exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,39 @@
+from fastapi import HTTPException, status
+
+
+class RentsException(HTTPException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(RentsException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "User already exists"
+
+
+class UserNotExistsException(RentsException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "User not exists"
+
+
+class PasswordNotCorrectException(RentsException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Password not correct"
+
+
+class TokenNotExistsException(RentsException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Token not exists"
+
+
+class InvalidTokenFormatException(RentsException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Invalid token format"
+
+
+class ExpiredTokenException(RentsException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Expired token"
