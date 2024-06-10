beadc57e06f12c0a6d245ac6c2d8f3a19776d8e0
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-06-10 12:13:57 +0300

    add delete_rent router

--- app/rents/router.py
+++ app/rents/router.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends
 from datetime import date
 
+from pydantic.tools import parse_obj_as
+
 from app.exceptions import CarCanNotBeRented
 from app.rents.schemas import SRents
 from app.rents.service import RentService
 from app.users.model import Users
 from app.users.dependencies import get_current_user
+from app.tasks.tasks import send_rent_confirmation_email
 
 router = APIRouter(
     prefix="/rents",
@@ -19,8 +22,16 @@ async def get_rents(user: Users = Depends(get_current_user)) -> list[SRents]:
 
 
 @router.post("")
-async def test_get_rents(car_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
-    rents = await RentService.create_new_rent(car_id, user.id, date_from, date_to)
-    if not rents:
+async def create_rent(car_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)) :
+    rent = await RentService.create_new_rent(car_id, user.id, date_from, date_to)
+    rent_dict = parse_obj_as(SRents, rent).dict()
+    send_rent_confirmation_email.delay(rent_dict, user.email)
+
+    if not rent:
         raise CarCanNotBeRented
-    return rents
+    return rent_dict
+
+
+@router.delete("/rent_id")
+async def delete_rent(rent_id: int, user: Users = Depends(get_current_user)):
+    return await RentService.delete(id=rent_id, user_id=user.id)
