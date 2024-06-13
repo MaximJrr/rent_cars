6d3936172f0828be95c8c11eeaa20165fcba4883
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-06-13 14:36:10 +0300

    add router for create images

---
+++ app/images/router.py
@@ -0,0 +1,17 @@
+import shutil
+
+from fastapi import UploadFile, APIRouter
+
+
+router = APIRouter(
+    prefix="/images",
+    tags=["Load images"]
+)
+
+
+@router.post("/images")
+async def add_cars(name: int, file: UploadFile):
+    with open(f"app/static/images/{name}.webp", "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+
+
