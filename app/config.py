b62b26c9aa88bbec5c592e790e1970efa4ea40dd
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-07-01 14:42:11 +0300

    create new func, add attributes for test database

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 from pydantic import BaseSettings, root_validator
+from typing import Literal
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -12,6 +15,25 @@ class Settings(BaseSettings):
     def get_database_url(cls, v):
         v["DATABASE_URL"] = f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
         return v
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: int
+    TEST_DB_NAME: str
+
+    @root_validator
+    def get_test_database_url(cls, v):
+        v["TEST_DATABASE_URL"] = f"postgresql+asyncpg://{v['TEST_DB_USER']}:{v['TEST_DB_PASS']}@{v['TEST_DB_HOST']}:{v['TEST_DB_PORT']}/{v['TEST_DB_NAME']}"
+        return v
+
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASS: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
 
     SECRET_KEY: str
     ALGORITHM: str
