f79e8ec7f2d395e8e8bbf4e0b50626052407636b
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-07-22 14:37:21 +0300

    add tests for cars

---
+++ app/tests/integration_tests/test_cars/test_api_cars.py
@@ -0,0 +1,13 @@
+import pytest
+from httpx import AsyncClient
+from fastapi import status
+
+
+@pytest.mark.parametrize("name, status_code", [
+    ("BMW", status.HTTP_200_OK),
+    ("error", status.HTTP_409_CONFLICT)
+])
+async def test_get_cars(name, status_code, async_client: AsyncClient):
+    request = await async_client.get(f"/cars/{name}")
+
+    assert request.status_code == status_code
