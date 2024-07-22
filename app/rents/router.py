334b45cdd3b20da22b5d88d0ff47b0d7e9369520
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-07-22 14:43:20 +0300

    fixed bug in path parametr

--- app/rents/router.py
+++ app/rents/router.py
@@ -3,7 +3,7 @@ from datetime import date
 
 from pydantic.tools import parse_obj_as
 
-from app.exceptions import CarCanNotBeRented
+from app.exceptions import CarCanNotBeRentedException
 from app.rents.schemas import SRents
 from app.rents.service import RentService
 from app.users.model import Users
@@ -22,16 +22,18 @@ async def get_rents(user: Users = Depends(get_current_user)) -> list[SRents]:
 
 
 @router.post("")
-async def create_rent(car_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)) :
+async def create_rent(car_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
     rent = await RentService.create_new_rent(car_id, user.id, date_from, date_to)
-    rent_dict = parse_obj_as(SRents, rent).dict()
-    send_rent_confirmation_email.delay(rent_dict, user.email)
 
     if not rent:
-        raise CarCanNotBeRented
+        raise CarCanNotBeRentedException
+
+    rent_dict = parse_obj_as(SRents, rent).dict()
+    send_rent_confirmation_email.delay(rent_dict, user.email)
     return rent_dict
 
 
-@router.delete("/rent_id")
+@router.delete("/{rent_id}")
 async def delete_rent(rent_id: int, user: Users = Depends(get_current_user)):
-    return await RentService.delete(id=rent_id, user_id=user.id)
+    await RentService.delete(id=rent_id, user_id=user.id)
+
