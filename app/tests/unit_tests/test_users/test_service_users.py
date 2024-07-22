a511480c07c73d17d836e3b0ab03c0faf0889c07
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-07-22 14:39:34 +0300

    add unit tests for users

---
+++ app/tests/unit_tests/test_users/test_service_users.py
@@ -0,0 +1,18 @@
+from app.users.service import UserService
+import pytest
+
+
+@pytest.mark.parametrize("user_id, email, is_exists", [
+    (7, "user@example.com", True),
+    (6, "5sharik@moloko.ru", True),
+    (6656, "error", False)
+])
+async def test_get_user_by_id(user_id, email, is_exists):
+    user = await UserService.get_by_id(user_id)
+
+    if is_exists:
+        assert user
+        assert user.id == user_id
+        assert user.email == email
+    else:
+        assert not user
