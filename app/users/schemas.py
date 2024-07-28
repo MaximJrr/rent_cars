da016ad24261a6b45e6c2fb7a3f730e00bd07059
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-07-28 16:50:11 +0300

    created SUser, SUserUpdate, SUserUpdatePartial schemas for users

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,6 +1,37 @@
 from pydantic import BaseModel, EmailStr
 
 
+class SUser(BaseModel):
+    id: int
+    name: str
+    last_name: str
+    email: EmailStr
+    passport_id: str
+    phone_number: str
+
+    class Config:
+        orm_mode = True
+
+
 class SUsersAuth(BaseModel):
     email: EmailStr
     password: str
+
+
+class SUserUpdate(BaseModel):
+    name: str
+    last_name: str
+    email: EmailStr
+    passport_id: str
+    phone_number: str
+
+
+class SUserUpdatePartial(SUserUpdate):
+    name: str | None = None
+    last_name: str | None = None
+    email: EmailStr | None = None
+    passport_id: str | None = None
+    phone_number: str | None = None
+
+    class Config:
+        orm_mode = True
