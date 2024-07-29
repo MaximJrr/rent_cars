4b5b9a95349b0b140cba61c904ccddeb83ca879a
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-07-29 16:02:52 +0300

    created get_available_cars method

--- app/cars/service.py
+++ app/cars/service.py
@@ -1,6 +1,16 @@
 from app.cars.models import Cars
 from app.base_services.base import BaseService
+from app.database import async_session_maker
+
+from sqlalchemy import select
 
 
 class CarService(BaseService):
     model = Cars
+
+    @classmethod
+    async def get_available_cars(cls):
+        async with async_session_maker() as session:
+            query = select(cls.model).where(cls.model.quantity > 1)
+            result = await session.execute(query)
+            return result.scalars().all()
