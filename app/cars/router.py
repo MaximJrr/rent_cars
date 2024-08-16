41c83f4cc18abb458f36d8295ee8d3a413ccb0ce
Maxim Chukavov <maximchukavovmc@gmail.com>
2024-08-16 13:49:45 +0300

    add new router create_new_car

--- app/cars/router.py
+++ app/cars/router.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from fastapi_cache.decorator import cache
+from pydantic.tools import parse_obj_as
 
-from app.cars.schemas import SCars, ScarsUpdate
+from app.cars.schemas import SCars, SCarsCreate, ScarsUpdate
 from app.cars.service import CarService
 from app.exceptions import NoCarException
+from app.tasks.tasks import send_new_car_message_email
+from app.users.service import UserService
 
 router = APIRouter(
     prefix="/api/cars",
@@ -46,3 +50,18 @@ async def delete_car(car_id: int):
 @cache(expire=30)
 async def get_available_cars() -> list[SCars]:
     return await CarService.get_available_cars()
+
+
+@router.post("")
+async def create_new_car(cars: SCarsCreate):
+    new_car = await CarService.create_new(**cars.dict())
+    users = await UserService.get_all()
+    emails = []
+
+    for email in users:
+        emails.append(email.email)
+
+    car_dict = parse_obj_as(SCars, new_car).dict()
+    send_new_car_message_email.delay(car_dict, emails)
+
+    return JSONResponse(content={"message": "the car successfully created"})
